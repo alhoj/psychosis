f3c6d6c05ab08a03b55bd70295fe4dbb57db47ce
Jussi Alho <jussi.alho@aalto.fi>
2019-04-10 17:28:48 +0300

    added possibility to use atlas

--- demo_calculate_FC.m
+++ demo_calculate_FC.m
@@ -12,8 +12,10 @@ cfg.subs = {
 };
 
 cfg.cond='BL'; % use follow-up data for subjects of interest; options baseline 'BL' or follow-up 'FU'
-cfg.res='8mm'; % voxel resolution; options '2mm', '4mm', '8mm', '16mm', or '32mm'
+cfg.res='2mm'; % voxel resolution; options '2mm', '4mm', '8mm', '16mm', or '32mm'
 cfg.mask=[]; % if empty, use the default MNI152 mask, e.g. "MNI152_T1_4mm_brain_mask.nii"
+cfg.atlas='brainnetome_atlas_w_cerebellum_v2.nii'; % define atlas; leave empty for voxel-to-voxel connectivity
+cfg.roiTC='PCA'; % how to extract the roi timecourse; options 'PCA' or 'mean'
 cfg.outdir='test'; % label of the output folder
 cfg.useNonSpatialSmoothedData=0;
 %%
